bc2de7f79e1db6a60873946b6d9d463b4e83d065
Charles Boyce <31250541+cboyce183@users.noreply.github.com>
2019-06-03 11:16:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -86,13 +86,9 @@ db.on('error', ()=> {console.log('---FAILED to connect to mongoose')});
 db.once('open', () => {
   console.log( '+++Connected to mongoose');
 });
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
+
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
